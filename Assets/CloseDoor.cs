424c3f4f778a18c75e202440f9fcfc4feaadbe1f
Kaleth Cristancho <48393210+KaJoseh@users.noreply.github.com>
2021-04-26 18:56:31 -0500

    Room3 structure done

--- Assets/CloseDoor.cs
+++ Assets/CloseDoor.cs
@@ -25,7 +25,25 @@ public class CloseDoor : MonoBehaviour
             //print("Cerrando");
             if (gm.doorOpened)
                 gm.doorOpened = false;
-            gm.currentRoom = GameManager.Rooms.room2;
+            //gm.currentRoom = GameManager.Rooms.room2;
+
+            switch (gm.currentRoom)
+            {
+                case GameManager.Rooms.room1:
+                    gm.currentRoom = GameManager.Rooms.room2;
+                    break;
+                case GameManager.Rooms.room2:
+                    gm.currentRoom = GameManager.Rooms.room3;
+                    break;
+                case GameManager.Rooms.room3:
+                    gm.currentRoom = GameManager.Rooms.room4;
+                    break;
+                case GameManager.Rooms.room4:
+                    gm.currentRoom = GameManager.Rooms.room5;
+                    break;
+                case GameManager.Rooms.room5:
+                    break;
+            }
 
         }
     }
--- Assets/KeyitemManager.cs
+++ Assets/KeyitemManager.cs
@@ -1,10 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
-using TMPro;
+using UnityEngine.UI;
 
-public class noteTextManager : MonoBehaviour
+public class KeyitemManager : MonoBehaviour
 {
-    public TextMeshProUGUI noteText;
-
+    public Sprite[] keySprite;
+    public GameObject keyItem;
     GameManager gm;
 
     // Start is called before the first frame update
@@ -19,16 +21,19 @@ public class noteTextManager : MonoBehaviour
         switch (gm.currentRoom)
         {
             case GameManager.Rooms.room1:
-                noteText.SetText("Open the door");
+                keyItem.GetComponent<Image>().sprite = keySprite[0];
                 break;
             case GameManager.Rooms.room2:
-                noteText.SetText("I have lost my eye, give it back to me.");
+                keyItem.GetComponent<Image>().sprite = keySprite[1];
                 break;
             case GameManager.Rooms.room3:
+                keyItem.GetComponent<Image>().sprite = keySprite[2];
                 break;
             case GameManager.Rooms.room4:
+                keyItem.GetComponent<Image>().sprite = keySprite[3];
                 break;
             case GameManager.Rooms.room5:
+                keyItem.GetComponent<Image>().sprite = keySprite[4];
                 break;
         }
     }
